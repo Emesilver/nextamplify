a4393cd7e275d3f9f0b841061eda2141b20082e0
Emerson Silverio <emesilver@yahoo.com>
2023-01-04 15:16:03 -0300

    First commit with amplify api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nextamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
